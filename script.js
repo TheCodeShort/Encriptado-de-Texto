3d67b3198b0b5ec99afb5691ed181cc2efd17c42
ranndy <ranndy180@gmail.com>
2024-08-19 21:11:30 -0500

    actualizacion mejora de hacentos y caracteres especiales

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 /* de esta manera haces un evento, cuando se haga un click en textarea se van a desapareser ciertas cosas */
 
 function advertenciaTexto(){
-    document.getElementById("encritadoTexto").addEventListener("click", () =>{
+    document.getElementById("encritadoTexto").addEventListener("click", () =>{/* esta es una funcion de flecha () =>{} */
         let personaImg = document.getElementById("personaImg");
         let TextoInformativo = document.getElementById("TextoInformativo");
         personaImg.style.display = "none";
@@ -10,12 +10,14 @@ function advertenciaTexto(){
     });
 }
 
+
+
 function encritarBoton() {
 
     let noEncriptadoTextoBoton = document.getElementById("normalTexto").value.toLowerCase();
     let personaImg = document.getElementById("personaImg");
     let TextoInformativo = document.getElementById("TextoInformativo");
-    
+    let caracteres = /^[a-z-\n-""  ]+$/;/* +: Indica que puede haber uno o más de los caracteres permitidos. */
     /*.replace(/e/gi "") nos cambia una letra y el primer parametro me esta diciendo que vamos a cambiar la letra 
     e por la palabra enter que es lo que se pone entre comillas */
     let cifradoTexto = noEncriptadoTextoBoton
@@ -24,11 +26,21 @@ function encritarBoton() {
                                             .replace(/a/gi, "ai")
                                             .replace(/o/gi, "ober")
                                             .replace(/u/gi, "ufat");
-    if(noEncriptadoTextoBoton.length != 0){
+
+
+    if (noEncriptadoTextoBoton.length != 0){
         document.getElementById("encritadoTexto").value = cifradoTexto;
         document.getElementById("normalTexto").value = "";
         TextoInformativo.style.display = "none";
         personaImg.style.display = "none";
+
+    }else if(!caracteres.test(noEncriptadoTextoBoton)) {
+            alert("El campo de del texto sin encriptar no debe contener caracteres especiales ni numeros ");
+            document.getElementById("normalTexto").value = "";
+            document.getElementById("encritadoTexto").value = "";
+            personaImg.style.display = "block";
+            TextoInformativo.style.display = "flex";
+
     }else{
         alert("No se detecto un mensaje para encriptar");
         personaImg.style.display = "block";
@@ -37,6 +49,7 @@ function encritarBoton() {
     }
     return;
 }
+
 
 
 function desencritarBoton(){
@@ -60,13 +73,30 @@ function desencritarBoton(){
         alert("No se encontro un texto para desencriptar");
         personaImg.style.display = "block";
         TextoInformativo.style.display = "flex";
-        document.getElementById("normalTexto").value = "";
+        /* document.getElementById("normalTexto").value = ""; */
     }
     return;
 }
 
+
+function copiarBoton(){
+    let EncriptadoTextoBoton = document.getElementById("encritadoTexto");
+    if(EncriptadoTextoBoton.length != 0){
+        EncriptadoTextoBoton.select();
+        EncriptadoTextoBoton.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(EncriptadoTextoBoton.value);/* navigator.clipboard es parte de la Clipboard API, que proporciona acceso para leer y escribir en el portapapeles del sistema de forma segura. */
+        alert(`texto copiado ${EncriptadoTextoBoton.value}`);
+        console.log(`El texto a sido copiado ${EncriptadoTextoBoton}`);
+        
+    }else{
+        alert("No se encontro un texto para copiar");
+        personaImg.style.display = "block";
+        TextoInformativo.style.display = "flex";
+    }
+    return;
+}   
 advertenciaTexto();
-/* 
+/*
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
