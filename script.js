8ad7c87c20a959976ba00cd9347d100f7e7d4dc5
ranndy <ranndy180@gmail.com>
2024-08-19 21:25:25 -0500

    actualizacion pequeña a la parte anterior

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function encritarBoton() {
     let noEncriptadoTextoBoton = document.getElementById("normalTexto").value.toLowerCase();
     let personaImg = document.getElementById("personaImg");
     let TextoInformativo = document.getElementById("TextoInformativo");
-    let caracteres = /^[a-z-\n-""  ]+$/;/* +: Indica que puede haber uno o más de los caracteres permitidos. */
+    let caracteres = /^[a-z-\n  ]+$/;/* +: Indica que puede haber uno o más de los caracteres permitidos. */
     /*.replace(/e/gi "") nos cambia una letra y el primer parametro me esta diciendo que vamos a cambiar la letra 
     e por la palabra enter que es lo que se pone entre comillas */
     let cifradoTexto = noEncriptadoTextoBoton
@@ -28,19 +28,18 @@ function encritarBoton() {
                                             .replace(/u/gi, "ufat");
 
 
-    if (noEncriptadoTextoBoton.length != 0){
+    if(!caracteres.test(noEncriptadoTextoBoton)) {
+        alert("El campo de del texto sin encriptar no debe contener caracteres especiales ni numeros ");
+        document.getElementById("normalTexto").value = "";
+        document.getElementById("encritadoTexto").value = "";
+        personaImg.style.display = "block";
+        TextoInformativo.style.display = "flex";
+
+    }else if(noEncriptadoTextoBoton.length != 0){
         document.getElementById("encritadoTexto").value = cifradoTexto;
         document.getElementById("normalTexto").value = "";
         TextoInformativo.style.display = "none";
         personaImg.style.display = "none";
-
-    }else if(!caracteres.test(noEncriptadoTextoBoton)) {
-            alert("El campo de del texto sin encriptar no debe contener caracteres especiales ni numeros ");
-            document.getElementById("normalTexto").value = "";
-            document.getElementById("encritadoTexto").value = "";
-            personaImg.style.display = "block";
-            TextoInformativo.style.display = "flex";
-
     }else{
         alert("No se detecto un mensaje para encriptar");
         personaImg.style.display = "block";
@@ -56,7 +55,7 @@ function desencritarBoton(){
     let EncriptadoTextoBoton = document.getElementById("encritadoTexto").value.toLowerCase();
     let personaImg = document.getElementById("personaImg");
     let TextoInformativo = document.getElementById("TextoInformativo");
-
+    let caracteres = /^[a-z-\n  ]+$/;
     let cifradoTexto = EncriptadoTextoBoton
                                             .replace(/enter/gi, "e")
                                             .replace(/imes/gi, "i")
@@ -64,16 +63,24 @@ function desencritarBoton(){
                                             .replace(/ober/gi, "o")
                                             .replace(/ufat/gi, "u");
 
-    if(EncriptadoTextoBoton.length != 0){
+    if(!caracteres.test(EncriptadoTextoBoton)) {
+        alert("El campo de del texto encriptado no debe contener caracteres especiales ni numeros ");
+        document.getElementById("normalTexto").value = "";
+        document.getElementById("encritadoTexto").value = "";
+        personaImg.style.display = "block";
+        TextoInformativo.style.display = "flex";
+
+    }else if (EncriptadoTextoBoton.length != 0){
         document.getElementById("normalTexto").value = cifradoTexto;
         document.getElementById("encritadoTexto").value = "";
         TextoInformativo.style.display = "none";
         personaImg.style.display = "none";
+
     }else{
         alert("No se encontro un texto para desencriptar");
         personaImg.style.display = "block";
         TextoInformativo.style.display = "flex";
-        /* document.getElementById("normalTexto").value = ""; */
+        document.getElementById("normalTexto").value = "";
     }
     return;
 }
